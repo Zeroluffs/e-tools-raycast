0490570d33116fb646b09ed8f4d91778419e3bd4
jhonerdlamarck <jpdlamarck@gmail.com>
2026-05-07 10:47:34 -0500

    show details/hide details

--- src/etools.tsx
+++ src/etools.tsx
@@ -1,11 +1,13 @@
 import { List, ActionPanel, Action } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
+import { useState } from "react";
 
 export interface CommandInterface {
   id: string;
   title: string;
   subtitle: string;
   command: string;
+  markdown: string;
 }
 export interface Section {
   id: string;
@@ -18,12 +20,18 @@ export interface CommandsObject {
   commands: Section[];
 }
 export default function Command() {
+  const [showDetail, setShowDetail] = useState(false);
   const { data, isLoading } = useCachedPromise(async () => {
     const res = await fetch("https://raw.githubusercontent.com/Zeroluffs/e-comands/main/commands.json");
     return res.json() as Promise<CommandsObject>;
   });
   return (
-    <List isLoading={isLoading} navigationTitle="Search Commands" searchBarPlaceholder="Search Encompass Commands">
+    <List
+      isShowingDetail={showDetail}
+      isLoading={isLoading}
+      navigationTitle="Search Commands"
+      searchBarPlaceholder="Search Encompass Commands"
+    >
       {data?.commands?.map((section) => (
         <List.Section key={section.id} title={section.sectionTitle} subtitle={section.subtitle}>
           {section.commands.map((command) => (
@@ -32,8 +40,15 @@ export default function Command() {
               title={command.title}
               subtitle={command.subtitle}
               accessories={[{ text: command.command }]}
+              detail={<List.Item.Detail markdown={command.markdown}></List.Item.Detail>}
               actions={
                 <ActionPanel>
+                  <Action
+                    onAction={() => setShowDetail((value) => !value)}
+                    title={showDetail ? "Hide Details" : "Show Details"}
+                    shortcut={{ modifiers: ["cmd"], key: "d" }}
+                  />
+
                   <Action.Paste content={command.command} />
                 </ActionPanel>
               }
